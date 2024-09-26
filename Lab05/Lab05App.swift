3954f432f1e5f87cdbfcd06453fb1eec8da18df5
mitrajesse <mitrajesse@gmail.com>
2024-09-26 18:06:39 -0400

    Initial Commit

---
+++ Lab05/Lab05App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab05App.swift
+//  Lab05
+//
+//  Created by Jesse Mitra on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
